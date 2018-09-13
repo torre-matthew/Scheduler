fc27f8eef4d9c71edd3ef61e004bb715cab09bb3
Torre Matthew <torre.matthew@gmail.com>
2018-09-12 20:36:08 -0400

    added intro text and comments to js file

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -10,79 +10,69 @@ let config = {
   firebase.initializeApp(config);
 
   let dbRef = firebase.database();
-  let trainName = "";
+  let carName = "";
   let destination = "";
   let frequency = "";
-  let firstTrainTime = "";
+  let firstCarTime = "";
   let minsAway = "";
   let nextArrival = "";
 
+  //When the form in the modal is submitted, "push" data to the firebase db. Also, add values that live in the databse (not calculated) to the page.
   $(".submit-form").on("click", function (event) {
 
-    trainName = $("#name-input").val().trim();
+    carName = $("#name-input").val().trim();
     destination = $("#destination-input").val().trim();
     frequency = $("#frequency-input").val().trim();
-    firstTrainTime = $("#time-input").val().trim();
+    firstCarTime = $("#time-input").val().trim();
     
     dbRef.ref().push({
-        train: trainName,
+        car: carName,
         destination: destination,
         frequency: frequency,
-        firstTrainTime: firstTrainTime
+        firstCarTime: firstCarTime
     });
 
     $("#name-input").val("");
     $("#destination-input").val("");
     $("#frequency-input").val("");
     $("#time-input").val("");
-
-
-        console.log(trainName);
-        console.log(destination);
-        console.log(firstTrainTime);
-        console.log(frequency);
         
 
   });
 
+  //Logic for calculating First Arrival time and mins away based on values sotred in the db.
   function timeCalc () {
     
-    let momentFirstTrainTime = moment(firstTrainTime, "hh:mm A");
+    let momentFirstTrainTime = moment(firstCarTime, "hh:mm A");
     let minsBetweenFirstTimeAndNow = moment().diff(momentFirstTrainTime, "minutes");
     let runs = Math.floor(minsBetweenFirstTimeAndNow/frequency);
         nextArrival = momentFirstTrainTime.add(runs*frequency, "m");
         minsAway = nextArrival.diff(moment(), "minutes");
 
+    // If minsAway has passed, calculate the minsAway for the next Arrival time.
     if (minsAway < 0) {
         nextArrival = nextArrival.add(frequency, "m");
-        console.log("Next Arrival Time " + nextArrival.format("hh:mm A"));
-        console.log("Next Arrival Time " + nextArrival);
         minsAway = nextArrival.diff(moment(), "minutes");
-        console.log("Minutes Away " + minsAway);
     } 
     
+    // If car is arriving now, say so!
     if (minsAway === 0) {
         minsAway = "Ariving Now";
     }
   }
 
+  //Whenever new values are added to the db and on first page load, display what exists in the database at that time and calculate the necessary values based on that data.
   dbRef.ref().on("child_added", function (newEntrySnapshot) {
-    // console.log(newEntrySnapshot.val().train);
-    // console.log(newEntrySnapshot.val().destination);
-    console.log(newEntrySnapshot.val().firstTrainTime);
-    // console.log(newEntrySnapshot.val().frequency);
-
-    firstTrainTime = newEntrySnapshot.val().firstTrainTime; 
+    firstCarTime = newEntrySnapshot.val().firstCarTime; 
     frequency = newEntrySnapshot.val().frequency;
 
     timeCalc ();
-    
 
     nextArrival = nextArrival.format("hh:mm A");
 
 
     $("tbody").append("<tr><td>" + 
-        newEntrySnapshot.val().train + 
+        newEntrySnapshot.val().car + 
         "</td><td>" + 
         newEntrySnapshot.val().destination + 
         "</td><td>" + 
